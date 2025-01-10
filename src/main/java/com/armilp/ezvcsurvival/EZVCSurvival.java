ab8080cd7549609d1e253330a6a760da4bb3829d
armilp <arnaugr02@gmail.com>
2025-01-10 16:29:14 +0100

    - Unused imports

--- src/main/java/com/armilp/ezvcsurvival/EZVCSurvival.java
+++ src/main/java/com/armilp/ezvcsurvival/EZVCSurvival.java
@@ -1,18 +1,14 @@
 package com.armilp.ezvcsurvival;
 
-import com.armilp.ezvcsurvival.config.FollowVoiceConfig;
-import com.armilp.ezvcsurvival.config.FollowVoiceGoalInjector;
+import com.armilp.ezvcsurvival.config.VoiceConfig;
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -25,7 +21,7 @@ public class EZVCSurvival {
     public EZVCSurvival() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, FollowVoiceConfig.CONFIG);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, VoiceConfig.CONFIG);
 
         // Registra eventos
         modEventBus.addListener(this::commonSetup);
